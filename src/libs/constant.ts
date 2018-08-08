5b357455844be66cf19ba4e4361e74b0928c9c90
ssddi456 <torrent88410@gmail.com>
2018-08-08 17:16:48 +0800

    split go home and go work

--- src/libs/constant.ts
+++ src/libs/constant.ts
@@ -4,5 +4,5 @@ export const dataDir = path.join(__dirname, '../../public/data');
 export const timeReg = /\d{4}-\d{2}-\d{2}_\d{2}-\d{2}/;
 export const goWorkDataFileNameReg = /^go_work_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}.json$/;
 export const goHomeDataFileNameReg = /^go_home_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}.json$/;
-export const goWorkPrefix = 'go_work_';
-export const goHomePrefix = 'go_home_';
+export const goWorkPrefix = 'go_work';
+export const goHomePrefix = 'go_home';
